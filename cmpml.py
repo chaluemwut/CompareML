42342e25b28a52706906b24ec21b10d540e796ea
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-02-01 20:24:20 +0700

    m=100

--- cmpml.py
+++ cmpml.py
@@ -10,11 +10,12 @@ from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, Gradient
 from sklearn import svm
 import logging
 import numpy as np
+import pickle
 
 logging.basicConfig(level=logging.INFO)
 
 def str_rf(self):
-    return 'Random Forest'
+    return 'RandomForest'
 
 def str_svm(self):
     return 'svm'
@@ -26,7 +27,7 @@ def str_boosted(self):
     return 'Boosted'
 
 def str_nb(self):
-    return 'Naive Bayes'
+    return 'NaiveBayes'
 
 def create_letter_p2():
     import string
@@ -70,15 +71,35 @@ class CmpML(object):
         logging.debug(lst_score)
         max_index = lst_score.index(max(lst_score))
         return lst_model[max_index]
-    
+
+    def select_best_model_score(self, m, x, y):
+        from sklearn.cross_validation import KFold
+        from sklearn.metrics import accuracy_score
+        import copy
+        kf = KFold(len(y), n_folds=5)
+        lst_score = []
+        lst_model = []
+        for train, test in kf:
+            m_i = copy.deepcopy(m)
+            x_train, y_train = x[train], y[train]
+            x_test, y_test = x[test], y[test]
+            m_i = m_i.fit(x_train, y_train)
+            y_pred = m_i.predict(x_test)
+            score = accuracy_score(y_test, y_pred)
+            lst_score.append(score)
+            lst_model.append(m_i)
+        logging.debug(lst_score)
+        max_index = lst_score.index(max(lst_score))
+        return lst_model[max_index], lst_score[max_index]
+
     def report_by_metrics(self, data_map, header, ml):
-        print "---------------  Report by metrics  -----------------------------"
+        print "----------------------------------------------------  Report by metrics  --------------------------------------------"
         str = "{:<14} | ".format("model")
         for h in header:
             str+="{:<14} | ".format(h)
         str+="mean"
         print str
-        print "------------------------------------------------------------------"
+        print "---------------------------------------------------------------------------------------------------------------------"
         for key in ml:
             value = data_map[key]
             str = "{:<14} | ".format(key)
@@ -96,26 +117,27 @@ class CmpML(object):
                                                            lst_value[3],
                                                            lst_value[4],
                                                            # lst_value[5],
-                                                           np.average(lst_value))
+                                                           np.mean(lst_value))
             print str
     
     def report_by_datasets(self, data_map, header, ml):
-        print "------------------- Report by name ------------------------------"
+        print "--------------------------------------- Report by name ---------------------------------------------"
         str = "{:<14} | ".format("model")
         for h in header:
             str+="{:<14} | ".format(h)
         str+="mean"
         print str
-        print "------------------------------------------------------------------"
+        print "----------------------------------------------------------------------------------------------------"
         for key in ml:
             value = data_map[key]
             str = "{:<14} | ".format(key)
-            str += "{:<14} | {:<14} | {:<14} | {:<14} | {:<14} | {:<14}".format(np.mean(value[0]),
+            str += "{:<14} | {:<14} | {:<14} | {:<14} | {:<14}".format(np.mean(value[0]),
                                                            np.mean(value[1]),
                                                            np.mean(value[2]),
                                                            np.mean(value[3]),
-                                                           np.mean(value[4]),
-                                                           np.mean(value))
+                                                           # np.mean(value[4]),
+                                                           np.mean([value[0], value[1], value[2], value[3]])
+            )
             print str
 
 
@@ -136,29 +158,98 @@ class CmpML(object):
             str += "{}".format(np.mean(value))
             print str
             print "------------------------------------------------------------------"
-        
-    def process_cmp_new(self):
-        datasets = ['adult','cov_type','letter.p1','letter.p2', 'fbcredibility']
+
+    def save_model_nb(self):
+        self.init_setup()
+        sd = SDDataSets()
+        # for m in self.ml:
+        for data_name in self.datasets:
+            m = GaussianNB()
+            x_train, y_train, x_test, y_test = sd.load(data_name)
+            model, predict_score = self.select_best_model_score(m, x_train, y_train)
+            file_name = "model/{}_{}".format('NaiveBayes', data_name)
+            print '{}'.format(predict_score)
+            pickle.dump(model, open(file_name, 'wb'))
+
+    def save_model(self):
+        self.init_setup()
+        sd = SDDataSets()
+        # for m in self.ml:
+        for data_name in self.datasets:
+            lst_model = []
+            lst_score = []
+            # file_name = "model/{}_{}".format(m, data_name)
+            # print file_name
+            for i in range(1, 101):
+                m = GradientBoostingClassifier(n_estimators=i)
+                print 'i = ',i
+                x_train, y_train, x_test, y_test = sd.load(data_name)
+                model, predict_score = self.select_best_model_score(m, x_train, y_train)
+                lst_model.append(model)
+                lst_score.append(predict_score)
+            # print '{} {}'.format(b_score, b_model.n_estimators)
+            # print lst_score
+            file_name = "model/{}_{}".format('NaiveBayes', data_name)
+            max_index = lst_score.index(max(lst_score))
+            b_score = lst_score[max_index]
+            b_model = lst_model[max_index]
+            print '{} {}'.format(b_score, b_model.n_estimators)
+            pickle.dump(b_model, open(file_name, 'wb'))
+
+
+    def init_setup(self):
+        self.datasets = ['adult', 'cov_type', 'letter.p1', 'letter.p2']
         RandomForestClassifier.__str__ = str_rf
         svm.SVC.__str__ = str_svm
         BaggingClassifier.__str__ = str_bagging
         GradientBoostingClassifier.__str__ = str_boosted
         GaussianNB.__str__ = str_nb
-
-        ml = [GradientBoostingClassifier(n_estimators=1024),
-              RandomForestClassifier(n_estimators=1024),
-              BaggingClassifier(DecisionTreeClassifier(), n_estimators=100),
+        self.ml = [GradientBoostingClassifier(),
+              RandomForestClassifier(),
+              BaggingClassifier(DecisionTreeClassifier()),
               GaussianNB()]
-        # ml = [GradientBoostingClassifier(),
-        #       RandomForestClassifier(),
-        #       BaggingClassifier(DecisionTreeClassifier()),
+        # self.ml = [GradientBoostingClassifier(n_estimators=1024),
+        #       RandomForestClassifier(n_estimators=1024),
+        #       BaggingClassifier(DecisionTreeClassifier(), n_estimators=100),
         #       GaussianNB()]
+        # return self.datasets, ml
+
+    def process_save_model(self):
+        ml_name = ['Boosted', 'RandomForest', 'Bagging', 'NaiveBayes']
+        datasets = ['adult', 'cov_type', 'letter.p1', 'letter.p2']
+        sd = SDDataSets()
+        result = {}
+        for m_name in ml_name:
+            m_lst = []
+            for data_name in datasets:
+                lst = []
+                x_train, y_train, x_test, y_true = sd.load(data_name)
+                file_name = "model/{}_{}".format(m_name,data_name)
+                m = pickle.load(open(file_name, 'rb'))
+                y_pred = m.predict(x_test)
+
+                acc = accuracy_score(y_true, y_pred)
+                fsc = f1_score(y_true, y_pred)
+                roc_auc = roc_auc_score(y_true, y_pred)
+                apr = average_precision_score(y_true, y_pred)
+                rms = 1-mean_squared_error(y_true, y_pred)
+                # mxe = log_loss(y_true, y_pred, normalize=True)
+                lst.extend([acc, fsc, roc_auc, apr, rms])
+                m_lst.append(lst)
+            result[m_name] = m_lst
+        self.report_by_metrics(result,['acc', 'fsc', 'roc', 'apr', 'rms'], ml_name)
+        print ''
+        self.report_by_datasets(result, datasets, ml_name)
+
+
+    def process_cmp_new(self):
+        self.init_setup()
 
         result = {}   
         sd = SDDataSets()
-        for m in ml:
+        for m in self.ml:
             m_lst = []
-            for data_name in datasets:
+            for data_name in self.datasets:
 #                 print '++++++ data set ',data_name
                 lst = []
                 x_train, y_train, x_test, y_true = sd.load(data_name)
@@ -177,9 +268,9 @@ class CmpML(object):
                 lst.extend([acc, fsc, roc_auc, apr, rms])
                 m_lst.append(lst)
             result[m] = m_lst
-        self.report_by_metrics(result,['acc', 'fsc', 'roc', 'apr', 'rms'], ml)
+        self.report_by_metrics(result,['acc', 'fsc', 'roc', 'apr', 'rms'], self.ml)
         print ''
-        self.report_by_datasets(result, datasets, ml)
+        self.report_by_datasets(result, self.datasets, self.ml)
     
     def process_cmp(self):
 #         print 'process'
@@ -208,6 +299,8 @@ class CmpML(object):
 if __name__ == '__main__':
     logging.info('start...')
     cmpMl = CmpML()
-    cmpMl.process_cmp_new()
+    cmpMl.process_save_model()
+    # cmpMl.save_model_nb()
+    # cmpMl.process_cmp_new()
     logging.info('end...')
 
